a24891616f2dbe1f5ab81f161e0df11af3482f4f
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-18 01:10:31 +0200

    Implement router index with views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ImagesView from '../views/ImagesView.vue'
+import SearchView from '@/views/SearchView.vue'
+import BillingView from '@/views/BillingView.vue'
+import RankingView from '@/views/RankingView.vue'
+import SellersView from '@/views/SellersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +12,45 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        requireAuth: false
+      }
+    },
+    {
+      path: '/search/:query',
+      name: 'searchResults',
+      component: ImagesView,
+      props: (route) => ({
+        query: route.params.query
+      }),
+      meta: {
+        requireAuth: false
+      }
+    },
+    {
+      path: '/billing',
+      name: 'billing',
+      component: BillingView,
+      meta: {
+        requireAuth: false
+      }
+    },
+    {
+      path: '/ranking',
+      name: 'ranking',
+      component: RankingView,
+      meta: {
+        requireAuth: false
+      }
+    },
+    {
+      path: '/sellers',
+      name: 'sellers',
+      component: SellersView,
+      meta: {
+        requireAuth: false
+      }
     }
   ]
 })
