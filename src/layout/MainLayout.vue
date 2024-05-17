90dc34f1444baec011775a8aae45fc613032e5a7
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-18 01:14:31 +0200

    Create MainLayout

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import NavBar from "@/components/NavBar.vue"
+
+</script>
+<template>
+    <div class="flex">
+        <div class="w-full h-screen">
+            <NavBar />
+            <slot />
+        </div>
+    </div>
+
+</template>
