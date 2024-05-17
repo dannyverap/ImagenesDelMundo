5ae82b7f4a28e2e1b840fe157f08aed06fad556a
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-17 02:11:43 +0200

    Implement useSellerStore with Pinia and Integration with SellerService

---
+++ src/stores/Seller.ts
@@ -0,0 +1,60 @@
+import { defineStore } from 'pinia'
+import { SellerServiceInstance } from '@/services/SellerService'
+import type { ISeller } from '@/interfaces/ISeller'
+
+export const useSellerStore = defineStore('seller', {
+  state: () => {
+    return {
+      sellers: [] as ISeller[] | undefined,
+      sellerDetail: {} as ISeller | undefined
+    }
+  },
+  actions: {
+    async getSellers(): Promise<boolean> {
+      try {
+        const response = await SellerServiceInstance.getSellers()
+        this.sellers = response
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+    },
+    async getSeller(id: number): Promise<boolean> {
+      try {
+        const response = await SellerServiceInstance.getSeller(id)
+        this.sellerDetail = response
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+    async createSeller(data: Partial<ISeller>): Promise<boolean> {
+      try {
+        await SellerServiceInstance.createSeller(data)
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+    },
+    async editSeller(id: number, data: Partial<ISeller>): Promise<boolean> {
+      try {
+        await SellerServiceInstance.editSeller(id, data)
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+    },
+    async deleteSeller(id: number): Promise<boolean> {
+      try {
+        await SellerServiceInstance.deleteSeller(id)
+        return true
+      } catch (error) {
+        console.error(error)
+        return false
+      }
+    }
+  }
+})
