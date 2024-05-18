cfa9f8d175cfdde349535750aa42c28133933dc5
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-18 13:51:34 +0200

    Refactor Seller store to use reactive state

--- src/stores/Seller.ts
+++ src/stores/Seller.ts
@@ -1,68 +1,78 @@
 import { defineStore } from 'pinia'
 import { SellerServiceInstance } from '@/services/SellerService'
 import type { ISeller } from '@/interfaces/ISeller'
+import { ref, computed } from 'vue'
 
-export const useSellerStore = defineStore('seller', {
-  state: () => {
-    return {
-      sellers: [] as ISeller[] | undefined,
-      sellerDetail: {} as ISeller | undefined
+export const useSellerStore = defineStore('sellers', () => {
+  const sellers = ref([] as ISeller[] | undefined)
+  const sellerDetail = ref({} as ISeller | undefined)
+
+  const numberOfSeller = computed(() => {
+    if (!sellers.value) {
+      return 0
     }
-  },
-  getters: {
-    numberOfSeller(): number {
-      if (!this.sellers) {
-        return 0
-      }
-      return this.sellers.length
+    return sellers.value.length
+  })
+
+  const getSellers = async (): Promise<boolean> => {
+    try {
+      const response = await SellerServiceInstance.getSellers()
+      sellers.value = response
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
     }
-  },
-  actions: {
-    async getSellers(): Promise<boolean> {
-      try {
-        const response = await SellerServiceInstance.getSellers()
-        this.sellers = response
-        return true
-      } catch (error) {
-        console.error(error)
-        return false
-      }
-    },
-    async getSeller(id: number): Promise<boolean> {
-      try {
-        const response = await SellerServiceInstance.getSeller(id)
-        this.sellerDetail = response
-        return true
-      } catch (error) {
-        return false
-      }
-    },
-    async createSeller(data: Partial<ISeller>): Promise<boolean> {
-      try {
-        await SellerServiceInstance.createSeller(data)
-        return true
-      } catch (error) {
-        console.error(error)
-        return false
-      }
-    },
-    async editSeller(id: number, data: Partial<ISeller>): Promise<boolean> {
-      try {
-        await SellerServiceInstance.editSeller(id, data)
-        return true
-      } catch (error) {
-        console.error(error)
-        return false
-      }
-    },
-    async deleteSeller(id: number): Promise<boolean> {
-      try {
-        await SellerServiceInstance.deleteSeller(id)
-        return true
-      } catch (error) {
-        console.error(error)
-        return false
-      }
+  }
+
+  const getSeller = async (id: number): Promise<boolean> => {
+    try {
+      const response = await SellerServiceInstance.getSeller(id)
+      sellerDetail.value = response
+      return true
+    } catch (error) {
+      return false
     }
+  }
+
+  const createSeller = async (data: Partial<ISeller>): Promise<boolean> => {
+    try {
+      await SellerServiceInstance.createSeller(data)
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  const editSeller = async (id: number, data: Partial<ISeller>): Promise<boolean> => {
+    try {
+      await SellerServiceInstance.editSeller(id, data)
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  const deleteSeller = async (id: number): Promise<boolean> => {
+    try {
+      await SellerServiceInstance.deleteSeller(id)
+      return true
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  return {
+    sellers,
+    sellerDetail,
+    numberOfSeller,
+    getSellers,
+    getSeller,
+    createSeller,
+    editSeller,
+    deleteSeller
   }
 })
