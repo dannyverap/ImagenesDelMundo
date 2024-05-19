ace65ac88cafe6f51f723a8fa7d9e487dc1c0dc0
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-19 13:34:57 +0200

    Add RankingTable to RankingView

---
+++ src/views/RankingView.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import MainLayout from "@/layout/MainLayout.vue"
+import RankingTable from "@/components/RankingTable.vue"
+</script>
+<template>
+    <MainLayout>
+        <RankingTable/>
+    </MainLayout>
+</template>
