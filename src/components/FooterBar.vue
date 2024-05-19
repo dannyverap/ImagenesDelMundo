812b09b12ba7939cb7a0a62aaf9d18db2caf9c8b
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-20 00:53:11 +0200

    Create Footer

---
+++ src/components/FooterBar.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup></script>
+<template>
+    <nav class="bg-[#154759] text-white py-2 flex items-center justify-between px-6 shadow-md">
+        <div class="logo text-lg font-semibold text-white">
+            <a href="#" class="flex items-center space-x-3">
+                <img src="https://www.appdegestion.com/app/54_alegra/logo/54_alegra.png" class="h-5"
+                    alt="Imagenes del mundo Logo" />
+                <span class="self-center">ImagenesDelMundo</span>
+            </a>
+        </div>
+        <div class="social-icons flex space-x-4">
+            <a href="#" class="text-white hover:text-gray-200">
+                <i class="bi bi-linkedin"></i>
+            </a>
+            <a href="#" class="text-white hover:text-gray-200">
+                <i class="bi bi-instagram"></i>
+            </a>
+            <a href="#" class="text-white hover:text-gray-200">
+                <i class="bi bi-github"></i>
+            </a>
+        </div>
+    </nav>
+</template>
