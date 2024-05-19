4e7c18a9cdbe75bd88c6da2141a4fef48ebf396f
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-20 01:01:38 +0200

    Update Invoices store with new methods and totalPriceGiven Ref

--- src/stores/Invoices.ts
+++ src/stores/Invoices.ts
@@ -5,15 +5,35 @@ import { ref } from 'vue'
 
 export const useInvoicesStore = defineStore('Invoices', () => {
   const invoices = ref([] as IInvoice[] | undefined)
+  const totalPriceGiven = ref(0)
 
   const getInvoices = async (): Promise<boolean> => {
     try {
-      const response = await InvoiceServiceInstance.getInvoices()
-      invoices.value = response
+      const response = (await InvoiceServiceInstance.getInvoices()) as IInvoice[]
+      invoices.value = filterInvoicesWithSellers(response)
       return true
     } catch (error) {
       console.error(error)
       return false
+    }
+  }
+
+  const calculateTotalPrice = () => {
+    if (invoices.value) {
+      totalPriceGiven.value = invoices.value?.reduce(
+        (acc, invoice) => acc + (invoice.total || 0),
+        0
+      )
+    }
+  }
+
+  const filterInvoicesWithSellers = (invoices: IInvoice[]) => {
+    return invoices.filter((invoice) => invoice.seller !== null)
+  }
+
+  const getInvoicesBySellerId = (sellerId: number) => {
+    if (invoices.value) {
+      return invoices.value.find((invoice) => invoice.seller.id == sellerId)
     }
   }
   const createInvoice = async (data: IInvoice): Promise<boolean> => {
@@ -28,7 +48,10 @@ export const useInvoicesStore = defineStore('Invoices', () => {
 
   return {
     invoices,
+    totalPriceGiven,
     getInvoices,
-    createInvoice
+    createInvoice,
+    getInvoicesBySellerId,
+    calculateTotalPrice
   }
 })
