85f2fa6b151383ceeea86b35309d577ba99f0d2e
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-19 17:53:38 +0200

    Create InvoiceHelper

---
+++ src/utils/CreateInvoiceHelper.ts
@@ -0,0 +1,29 @@
+import type { IInvoice, IInvoiceProduct } from '@/interfaces/IInvoice'
+
+class InvoiceCreator {
+  formatDate(date: Date): string {
+    return date.toISOString().slice(0, 10)
+  }
+
+  createInvoiceItem(quantity: number): IInvoiceProduct {
+    return {
+      id: 2,
+      name: 'Search images service:',
+      price: 1,
+      quantity
+    }
+  }
+
+  createDefaultInvoice(sellerId: number, points: number): IInvoice {
+    const items = [this.createInvoiceItem(points)]
+    return {
+      items,
+      dueDate: this.formatDate(new Date()),
+      date: this.formatDate(new Date()),
+      client: 3,
+      seller: sellerId
+    }
+  }
+}
+
+export const InvoiceCreatorInstance = new InvoiceCreator()
