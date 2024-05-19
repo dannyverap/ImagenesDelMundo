8b957f072b7aa7d00c1c3e8d55b6a626fdc67050
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-20 01:03:36 +0200

    Implement sticky navigation and footer in MainLayout

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
 import NavBar from "@/components/NavBar.vue"
-
+import FooterBar from "@/components/FooterBar.vue";
 </script>
 <template>
-    <div class="flex">
-        <div class="w-full h-screen">
-            <NavBar />
-            <slot />
-        </div>
+    <div class="flex flex-col h-screen">
+      <NavBar class="sticky top-0 z-50" />
+      <div class="flex-grow">
+        <slot />
+      </div>
+      <FooterBar class="sticky bottom-0 z-50" />
     </div>
-
-</template>
+  </template>
+  
