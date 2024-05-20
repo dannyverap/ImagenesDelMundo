f3506bba463cf412228006553c62945e50bded03
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-20 23:43:55 +0200

    Create AppLoco Component

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import logo from "@/assets/alegra.png"
+</script>
+
+<template>
+  <div class="flex justify-center items-center mix-blend-multiply">
+    <img :src="logo" class="w-40" />
+  </div>
+</template>
