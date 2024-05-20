6b466aa094e0857954b50d1d040f910b15294426
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-20 13:25:08 +0200

    Add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#31ABA9',          // teal
+        primaryDark: '#509B9C',      // light teal
+        background: '#E1ECEF',       // light gray
+        textPrimary: '#4D5D73',      // dark blue
+        textSecondary: '#FFFFFF',    // white
+        hoverPrimary: '#CFF2F1',     // light cyan
+      },
+    }
   },
   plugins: []
 }
