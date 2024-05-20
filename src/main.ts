ba865ee0fe7fa0dd5f82479e57047f4aa98fcac2
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-21 00:02:39 +0200

    Add pinia-plugin-persistedstate for persistent data storage in Auth Store

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,17 @@ import App from './App.vue'
 import router from './router'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import Toast, { POSITION, type PluginOptions } from 'vue-toastification'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import 'vue-toastification/dist/index.css'
 const app = createApp(App)
 const options: PluginOptions = {
-    position: POSITION.BOTTOM_CENTER
+  position: POSITION.BOTTOM_CENTER
 }
 const pinia = createPinia()
 app.use(pinia)
 app.use(router)
 app.use(Toast, options)
+pinia.use(piniaPluginPersistedstate)
+
 app.mount('#app')
