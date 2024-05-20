fdbd194e26f094df134a69063d23ba3b543ec2da
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-20 23:45:26 +0200

    Create Auth store

---
+++ src/stores/Auth.ts
@@ -0,0 +1,66 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import { AuthServiceInstance } from '@/services/AuthService'
+
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref('')
+
+  const isAuthenticated = computed(() => !!token.value)
+
+  const signup = async (phone: string, email: string, password: string): Promise<boolean> => {
+    try {
+      const response = await AuthServiceInstance.Signup(phone, email, password)
+      if (response.data) {
+        token.value = response.data
+        return true
+      } else {
+        throw new Error(response.message)
+      }
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  const login = async (email: string, password: string): Promise<boolean> => {
+    try {
+      const response = await AuthServiceInstance.Login(email, password)
+      if (response.data) {
+        token.value = response.data
+        return true
+      } else {
+        throw new Error(response.message)
+      }
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  const logout = () => {
+    token.value = ''
+  }
+
+  const validateToken = async (): Promise<boolean> => {
+    try {
+      const response = await AuthServiceInstance.ValidateToken(token.value)
+      if (response.data) {
+        return true
+      } else {
+        throw new Error(response.message)
+      }
+    } catch (error) {
+      console.error(error)
+      return false
+    }
+  }
+
+  return {
+    token,
+    isAuthenticated,
+    signup,
+    login,
+    logout,
+    validateToken
+  }
+})
