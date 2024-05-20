e5d53ff25f3322cca78c003303c92cfa2e72bd69
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-20 22:48:57 +0200

    Integrate vue-toastification for notifications and configure positioning

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import Toast, { POSITION, type PluginOptions } from 'vue-toastification'
 
+import 'vue-toastification/dist/index.css'
 const app = createApp(App)
-
+const options: PluginOptions = {
+    position: POSITION.BOTTOM_CENTER
+}
 const pinia = createPinia()
 app.use(pinia)
 app.use(router)
-
+app.use(Toast, options)
 app.mount('#app')
