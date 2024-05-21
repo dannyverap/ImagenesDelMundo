8beb7b910ba75d65eed87c7edb08e11b46b30f2f
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-21 02:10:36 +0200

    Protect routes with auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,10 @@ import RankingView from '@/views/RankingView.vue'
 import RewardSummaryView from '@/views/RewardSummaryView.vue'
 import LoginView from '@/views/LoginView.vue'
 import SignupView from '@/views/SignupView.vue'
+import { useAuthStore } from '@/stores/Auth'
+import { useToast } from 'vue-toastification'
+
+const toast = useToast()
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -49,7 +53,7 @@ const router = createRouter({
       name: 'ranking',
       component: RankingView,
       meta: {
-        requireAuth: false
+        requireAuth: true
       }
     },
     {
@@ -57,10 +61,36 @@ const router = createRouter({
       name: 'rewardSummary',
       component: RewardSummaryView,
       meta: {
-        requireAuth: false
+        requireAuth: true
       }
     }
   ]
 })
+router.beforeEach(async (to, from, next) => {
+  const auth = useAuthStore()
+  const { isAuthenticated } = auth
+  const needAuth = to.meta.requireAuth
 
+  if (!needAuth) {
+    next()
+    return
+  }
+  if (!isAuthenticated) {
+    toast.warning('You need to be logged')
+    next({ name: 'login' })
+  }
+  if (isAuthenticated) {
+    try {
+      const isValidToken = await auth.validateToken()
+      if (!isValidToken) {
+        toast.error('Token invalido')
+        next({ name: 'login' })
+      }
+      next()
+    } catch (error) {
+      toast.error('Something went wrong')
+      next({ name: 'login' })
+    }
+  }
+})
 export default router
