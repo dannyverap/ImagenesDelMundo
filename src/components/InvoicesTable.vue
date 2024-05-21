28a40c5dc8c1d0e8ba7c361bfd214715dd939137
Danny Vera Palomino <danny.verap@gmail.com>
2024-05-21 14:50:38 +0200

    Fix invoice table responsiveness

--- src/components/InvoicesTable.vue
+++ src/components/InvoicesTable.vue
@@ -43,69 +43,92 @@ const sortedInvoices = computed(() => {
 </script>
 
 <template>
-  <div class="text-xl font-semibold mb-4 mt-9 text-center text-textPrimary">Reward Summary</div>
-  <div class="mt-8 flex justify-center overflow-x-auto">
-    <table class="w-full max-w-5xl text-sm text-left rtl:text-right text-gray-700">
-      <thead class="text-xs text-textPrimary bg-background uppercase">
-        <tr>
-          <th scope="col" class="px-3 py-3 rounded-tl-lg w-24">Day</th>
-          <th scope="col" class="px-3 py-3 w-48">Seller name</th>
-          <th scope="col" class="px-6 py-3 text-center">Seller ID</th>
-          <th scope="col" class="px-6 py-3 text-center">Invoice number</th>
-          <th scope="col" class="px-6 py-3 text-center rounded-tr-lg">Money earn</th>
-        </tr>
-      </thead>
-      <transition-group name="table-row" tag="tbody">
-        <template v-if="isLoading">
-          <tr v-for="n in 5" :key="n" class="animate-pulse">
-            <td class="px-3 py-4 whitespace-nowrap">
-              <div class="h-4 bg-gray-200 rounded"></div>
-            </td>
-            <td class="px-3 py-4">
-              <div class="h-4 bg-gray-200 rounded"></div>
-            </td>
-            <td class="px-6 py-4 text-center">
-              <div class="h-4 bg-gray-200 rounded"></div>
-            </td>
-            <td class="px-6 py-4 text-center">
-              <div class="h-4 bg-gray-200 rounded"></div>
-            </td>
-            <td class="px-6 py-4 text-center">
-              <div class="h-4 bg-gray-200 rounded"></div>
-            </td>
+  <div class="text-xl font-semibold mb-4 mt-9 text-center text-textPrimary">Seller's rewards</div>
+  <div class="mt-8 flex justify-center">
+    <div class="w-full max-w-5xl overflow-x-auto">
+      <table class="min-w-full text-sm text-left rtl:text-right text-gray-700">
+        <thead class="text-xs text-textPrimary bg-background uppercase">
+          <tr>
+            <th scope="col" class="px-3 py-3 w-24">Day</th>
+            <th scope="col" class="px-3 py-3 w-48">Seller name</th>
+            <th scope="col" class="px-6 py-3 text-center">Seller ID</th>
+            <th scope="col" class="px-6 py-3 text-center">Invoice number</th>
+            <th scope="col" class="px-6 py-3 text-center">Money earn</th>
           </tr>
-        </template>
-        <template v-else>
-          <tr v-for="(invoice, idx) in sortedInvoices" :key="invoice.id"
-            :class="[idx % 2 === 0 ? 'bg-gray-100' : 'bg-white']">
-            <th scope="row" class="px-3 py-4 font-medium text-textPrimary whitespace-nowrap">
-              {{ invoice.date }}
+        </thead>
+        <transition-group name="table-row" tag="tbody">
+          <template v-if="isLoading">
+            <tr v-for="n in 5" :key="n" class="animate-pulse">
+              <td class="px-3 py-4 whitespace-nowrap">
+                <div class="h-4 bg-gray-200 rounded"></div>
+              </td>
+              <td class="px-3 py-4">
+                <div class="h-4 bg-gray-200 rounded"></div>
+              </td>
+              <td class="px-6 py-4 text-center">
+                <div class="h-4 bg-gray-200 rounded"></div>
+              </td>
+              <td class="px-6 py-4 text-center">
+                <div class="h-4 bg-gray-200 rounded"></div>
+              </td>
+              <td class="px-6 py-4 text-center">
+                <div class="h-4 bg-gray-200 rounded"></div>
+              </td>
+            </tr>
+          </template>
+          <template v-else>
+            <tr
+              v-for="(invoice, idx) in sortedInvoices"
+              :key="invoice.id"
+              :class="[idx % 2 === 0 ? 'bg-gray-100' : 'bg-white']"
+            >
+              <th scope="row" class="px-3 py-4 font-medium text-textPrimary whitespace-nowrap">
+                {{ invoice.date }}
+              </th>
+              <td class="px-3 py-4 text-textPrimary w-48">{{ invoice.seller.name }}</td>
+              <td class="px-6 py-4 text-textPrimary text-center">{{ invoice.seller.id }}</td>
+              <td class="px-6 py-4 text-textPrimary text-center">
+                <a
+                  href="#"
+                  @click.prevent="invoice.id && getPDFandOpenIt(invoice.id)"
+                  class="text-blue-500 hover:text-blue-700 no-underline"
+                >
+                  {{ invoice.id }}
+                </a>
+              </td>
+              <td class="px-6 py-4 text-textPrimary text-center">${{ invoice.total }}</td>
+            </tr>
+          </template>
+        </transition-group>
+        <tfoot v-if="!isLoading">
+          <tr class="font-semibold text-textPrimary">
+            <th scope="row" class="px-6 py-3 text-base text-left" colspan="2">
+              Total Rewards Given
             </th>
-            <td class="px-3 py-4 text-textPrimary w-48">{{ invoice.seller.name }}</td>
-            <td class="px-6 py-4 text-textPrimary text-center">{{ invoice.seller.id }}</td>
-            <td class="px-6 py-4 text-textPrimary text-center">
-              <a href="#" @click.prevent="invoice.id && getPDFandOpenIt(invoice.id)"
-                class="text-blue-500 hover:text-blue-700 no-underline">
-                {{ invoice.id }}
-              </a>
-            </td>
-            <td class="px-6 py-4 text-textPrimary text-center">${{ invoice.total }}</td>
+            <td class="px-6 py-3"></td>
+            <td class="px-6 py-3"></td>
+            <td class="px-6 py-3 text-center">${{ invoiceStore.totalPriceGiven }}</td>
           </tr>
-        </template>
-      </transition-group>
-      <tfoot v-if="!isLoading">
-        <tr class="font-semibold text-textPrimary">
-          <th scope="row" class="px-6 py-3 text-base text-left" colspan="2">Total Rewards Given</th>
-          <td class="px-6 py-3"></td>
-          <td class="px-6 py-3"></td>
-          <td class="px-6 py-3 text-center">${{ invoiceStore.totalPriceGiven }}</td>
-        </tr>
-      </tfoot>
-    </table>
+        </tfoot>
+      </table>
+    </div>
   </div>
 </template>
 
 <style scoped>
+@media (max-width: 640px) {
+  table {
+    font-size: 0.875rem;
+    /* Reduce font size on smaller screens */
+  }
+
+  th,
+  td {
+    padding: 0.5rem;
+    /* Adjust padding for better fit on smaller screens */
+  }
+}
+
 .table-row-enter-active,
 .table-row-leave-active,
 .table-row-move {
